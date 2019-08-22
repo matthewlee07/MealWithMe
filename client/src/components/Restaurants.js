fc4afcfbde3d83f0de35db28f9cb9d8b3ef3e305
Matt Lee <matthewlee07@gmail.com>
2019-08-22 05:50:13 -0400

    refactored class to hooks in Restaurants

--- client/src/components/Restaurants.js
+++ client/src/components/Restaurants.js
@@ -1,41 +1,25 @@
-import React, { Component, Fragment } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import superagent from "superagent";
 
-export default class Restaurant extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      restaurants: []
-    };
-  }
-  // https://reactjs.org/docs/react-component.html#componentdidmount
-  componentDidMount() {
-    getRestaurants(this.props).then(restaurants =>
-      this.setState({ restaurants })
-    );
-  }
-  componentWillReceiveProps(newProps) {
-    if (this.props.search !== newProps.search) {
-      getRestaurants(newProps).then(restaurants =>
-        this.setState({ restaurants })
-      );
-    }
-  }
+export default function Restaurant(props) {
+  const [restaurants, setRestaurants] = useState([]);
+  useEffect(() => {
+    getRestaurants(props).then(setRestaurants);
+  }, [props.search]);
 
-  render() {
-    const restaurants = this.state.restaurants.map((restaurant, index) => (
-      <li className="restaurant" key={index}>
-        <span>{restaurant.name}</span>
-        <img src={restaurant.image_url} />
-      </li>
-    ));
-    return (
-      <Fragment>
-        <h1>The best restaurants in {this.props.search.location}</h1>
-        <ul>{restaurants}</ul>
-      </Fragment>
-    );
-  }
+  const restaurantsView = restaurants.map((restaurant, index) => (
+    <li className="restaurant" key={index}>
+      <span>{restaurant.name}</span>
+      <img src={restaurant.image_url} />
+    </li>
+  ));
+  return (
+    <Fragment>
+      <h1>The best restaurants in {props.search.location}</h1>
+      <ul>{restaurantsView}</ul>
+    </Fragment>
+  );
+  // }
 }
 // https://github.com/visionmedia/superagent
 function getRestaurants({ search: { location, category } }) {
