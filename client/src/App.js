f243ec5336ca71b21982937cbbf900e03ca4e31d
Matt Lee <matthewlee07@gmail.com>
2019-09-03 17:28:29 -0400

    .

--- client/src/App.js
+++ client/src/App.js
@@ -15,11 +15,13 @@ function App() {
 
   return (
     <div className="App">
-      {/* <header className="App-header">
+      <header className="App-header">
         <h1>Meal With Me</h1>
-      </header> */}
+      </header>
       <main>
-        <Search onSetSearch={setSearch} />
+        <span className="search">
+          <Search onSetSearch={setSearch} />
+        </span>
         <Restaurant search={search} />
       </main>
     </div>
