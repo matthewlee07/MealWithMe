23af3f7377f9366b29264c5473ea1078983227e2
Matt Lee <matthewlee07@gmail.com>
2019-09-05 05:59:29 -0400

    applying category filter

--- server/app/requests/yelp_request.rb
+++ server/app/requests/yelp_request.rb
@@ -5,8 +5,10 @@ class YelpRequest
       yelp_requests_path = "https://api.yelp.com/v3/businesses/search"
       # https://lostisland.github.io/faraday/usage/
       auth_header = "Bearer #{ENV['YELP_API_KEY']}"
+      categories = ['restaurants, All']
+      categories.push(category) if category.present?
       response = Faraday.get(yelp_requests_path, 
-        {location: location, categories: category}, 
+        {location: location, categories: categories.join(',')}, 
         {'Authorization': auth_header}
       )
   end
